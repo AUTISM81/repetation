b44fc889218e5b1208dd938aea0c545e573224d3
AUTISM81 <54669344+AUTISM81@users.noreply.github.com>
2020-08-25 13:40:25 +0200

    Dice roll

--- src/DiceRoll.java
+++ src/DiceRoll.java
@@ -1,2 +1,19 @@
 public class DiceRoll {
+    public static void main(String[] args) {
+        rolling();
+    }
+
+    public static void rolling(){
+        int count = 0;
+        for (int i = 0; i < 1;) {
+            int dice = (int) (Math.random() * 6 + 1) * 2;
+            count++;
+            System.out.println(dice);
+            if (dice == 2) {
+                i = 10;
+                System.out.println("it took " + count + " times to roll snake eyes");
+            }
+
+        }
+    }
 }
---
+++ src/diceFunction.java
@@ -0,0 +1,52 @@
+public class diceFunction {
+
+    public static void main(String[] args) {
+
+        int count1 = 0;
+        int count2 = 0;
+        int count3 = 0;
+        int count4 = 0;
+        int count5 = 0;
+        int count6 = 0;
+
+        for (int i = 0; i <= 10000; i++){
+            int dice = rolling();
+
+            System.out.println(dice);
+                               if (dice == 1) {
+                                   count1++;
+            } else             if (dice == 2) {
+                                   count2++;
+            } else             if (dice == 3) {
+                                   count3++;
+            } else             if (dice == 4) {
+                                   count4++;
+            } else             if (dice == 5) {
+                                   count5++;
+            } else             if (dice == 6) {
+                                   count6++;
+            }
+        }
+
+        float average;
+
+        average = count1/10000;
+        System.out.println("average for rolling " + "1" + " is " + average);
+        average = count2/10000;
+        System.out.println("average for rolling " + "2" + " is " + average);
+        average = count3/10000;
+        System.out.println("average for rolling " + "3" + " is " + average);
+        average = count4/10000;
+        System.out.println("average for rolling " + "4" + " is " + average);
+        average = count5/10000;
+        System.out.println("average for rolling " + "5" + " is " + average);
+        average = count6/10000;
+        System.out.println("average for rolling " + "6" + " is " + average);
+    }
+
+    public static int rolling(){
+            int dice = (int) (Math.random() * 6 + 1);
+
+            return dice;
+    }
+}
